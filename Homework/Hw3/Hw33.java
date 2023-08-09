9cc92f25f9e636751e3a184d1a177a226a27225c
Storm1108 <mail.laser@yandex.ru>
2023-08-09 23:27:18 +0300

    Hw33

--- Homework/Hw3/Hw33.java
+++ Homework/Hw3/Hw33.java
@@ -1,4 +1,26 @@
 package Homework.Hw3;
 
+import java.util.ArrayList;
+
 public class Hw33 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            array.add((int) (Math.random() * 20 - 10));
+        }
+        System.out.println(array);
+        Integer min = array.get(0);
+        Integer max = array.get(0);
+        float count = 0;
+        for (Integer elem :
+                array) {
+            if (elem < min) min = elem;
+            if (elem > max) max = elem;
+            count += elem;
+        }
+        System.out.printf("""
+                Минимальное значение: %d
+                Максимальное значение: %d
+                Среднее арифметическое: %,.2f""", min, max, count/array.size());
+    }
 }
